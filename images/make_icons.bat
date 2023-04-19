bf0d314cab814d173a78589063976d0e0cf7d78a
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-04-19 03:28:00 +0100

    Icons & Images & Projects

---
+++ images/make_icons.bat
@@ -0,0 +1,8 @@
+cls
+REM Names of the scripts
+set script_resize="../../Image Batch Processing Python Scripts/Image_Process_Scripts/resize.py" 
+
+REM ## Make the icons
+python "../../Image Batch Processing Python Scripts/Image_Process_Scripts/resize.py" 32 32 "./IconExports/" "."
+
+
