21be136da4536b21a3e8ec39a7b7db6edeb69ffe
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-11-06 00:41:33 +0000

    Create run_app.py

---
+++ run_app.py
@@ -0,0 +1,31 @@
+"""
+YouTube Video Downloader
+Copyright (C) 2023 JessyJP
+
+Author: JessyJP
+Date: April 7, 2023
+Description: This script downloads YouTube videos in the specified quality or allows the user to select a quality from the available streams.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent))
+
+from src.YouTubeDownloader import __main__
+
+if __name__ == '__main__':
+    __main__.start()
+
+# NOTE: if running this script returns an error, just run "YouTubeDownloader.py" directly
