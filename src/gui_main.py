69d3829105d6049a3ec7e9b0c6fac5302e7a82d3
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-11-06 21:56:03 +0000

    Image update
    Update gui_main.py

--- src/gui_main.py
+++ src/gui_main.py
@@ -194,7 +194,8 @@ class YouTubeDownloaderGUI(tk.Tk):
         self.theme["filename"] = theme_file
 
         application_window_icon_path = f"{self.theme['global']['directories']['icons']}{self.theme['icons']['application_window']}"
-        self.iconbitmap(application_window_icon_path)
+        print(os.getcwd())
+        # self.iconbitmap(os.path.join(os.getcwd(),application_window_icon_path))
 
         self.title(self.theme["window_title"])
         self.configure(bg=self.theme["global"]["colors"]["background"])
