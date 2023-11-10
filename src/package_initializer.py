68302a4dd223dc5339a70c1a5e9406513f7574d0
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-11-10 23:40:02 +0000

    Delete package_initializer.py
    Is now obsolete

--- src/package_initializer.py
+++
@@ -1,9 +0,0 @@
-## Check & Install packages
-from core import install_missing_modules
-
-install_missing_modules(["configparser","pillow","pyperclip"])
-install_missing_modules(["urlextract","pandas","requests"])
-install_missing_modules(["pytube","pytchat","tqdm","ffmpeg-python"])
-install_missing_modules(["beautifulsoup4"])
-install_missing_modules(["google-api-python-client","google_auth_oauthlib"])
-install_missing_modules(["youtube_dl"])
