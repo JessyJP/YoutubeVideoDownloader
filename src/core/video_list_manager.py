2a383aeba352fb7715e7400ba87ec8100520d2a5
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-11-10 01:25:11 +0000

    Update video_list_manager.py

--- src/core/video_list_manager.py
+++ src/core/video_list_manager.py
@@ -28,7 +28,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import threading
 from typing import List, Union
-from core.pytube_handler import VideoInfo
+from core.download_options import DownloadProgress
+from core.pytube_handler import LimitsAndPriority, VideoInfo
 import shutil
 
 
@@ -73,10 +74,51 @@ class VideoListManager:
 
     def getItemProp(index,property):
         pass
+    #TODO: not implemented
 
     def setRowProp(index,property):
         pass
+    #TODO: not implemented
 
+    ## --------------------------- Video info download ----------------------------------------
+
+    def video_item_process_download(self, n, limits, outputdir, outputExt):
+        # Define variables
+        N = len(self.infoList)
+        self.infoList[n].log(f"Process Entry Download {n+1} of {N}: ");
+        _DONE_ = "Done!"
+        _ERROR_ = "Error!"
+        _IN_PROGRESS_ = "Downloading Now..."
+
+        # Get the associated item
+        ui_item = self.get_tree_view_UI_item_by_video_info(self.infoList[n])
+        # initial_download_keep_str = self.tree.set(item, 'download_status')
+        def setUiItemStatus(ui_item, new_status=None):
+            self.tree.set(ui_item, 'download_status', new_status)
+        #end
+        
+        # TODO: get local limits here maybe. And if they exists apply them here. If not use global
+
+        # Start 
+        setUiItemStatus(ui_item, _IN_PROGRESS_)
+        try:
+            self.infoList[n].process_downloads_combine_keep(limits, outputdir, outputExt)                
+            self.infoList[n].download_status = _DONE_
+        except Exception as e:
+            print(f"File not finished. Error: {e}")
+            self.infoList[n].download_status = _ERROR_
+            temp_path = self.infoList[n].make_tmp_dir(outputdir)
+            shutil.rmtree(temp_path)
+        #end
+
+        # Global Update GUI
+        setUiItemStatus(ui_item, self.infoList[n].download_status)
+        count_done          = sum(1 for video_info in self.infoList if video_info.download_status == _DONE_)
+        count_in_progress   = sum(1 for video_info in self.infoList if video_info.download_status == _IN_PROGRESS_)
+        count_error         = sum(1 for video_info in self.infoList if video_info.download_status == _ERROR_)
+        self.update_progress(count_done+count_error,N,0)
+        self.dispStatus(f"Processing {N} item(s): Completed downloads {count_done} of {N}      Still in progress = {count_in_progress}, Errors = {count_error}!")
+    #end
 
     ## Process methods
     def downloadAllVideoItems(self, process_via_multithreading, limits, outputDir, outputExt):
@@ -88,51 +130,15 @@ class VideoListManager:
         # for info in self.infoList:
         for n in range(N):
 
-            def video_item_process_download(n, limits, outputdir, outputExt):
-                # Define variables
-                N = len(self.infoList)
-                self.infoList[n].log(f"Process Entry Download {n+1} of {N}: ");
-                _DONE_ = "Done!"
-                _ERROR_ = "Error!"
-                _IN_PROGRESS_ = "Downloading Now..."
 
-                # Get the associated item
-                ui_item = self.get_tree_view_UI_item_by_video_info(self.infoList[n])
-                # initial_download_keep_str = self.tree.set(item, 'download_status')
-                def setUiItemStatus(ui_item, new_status=None):
-                    self.tree.set(ui_item, 'download_status', new_status)
-                #end
-                
-                # TODO: get local limits here maybe. And if they exists apply them here. If not use global
-
-                # Start 
-                setUiItemStatus(ui_item, _IN_PROGRESS_)
-                try:
-                    self.infoList[n].process_downloads_combine_keep(limits, outputdir, outputExt)                
-                    self.infoList[n].download_status = _DONE_
-                except Exception as e:
-                    print(f"File not finished. Error: {e}")
-                    self.infoList[n].download_status = _ERROR_
-                    temp_path = self.infoList[n].make_tmp_dir(outputdir)
-                    shutil.rmtree(temp_path)
-                #end
-
-                # Global Update GUI
-                setUiItemStatus(ui_item, self.infoList[n].download_status)
-                count_done          = sum(1 for video_info in self.infoList if video_info.download_status == _DONE_)
-                count_in_progress   = sum(1 for video_info in self.infoList if video_info.download_status == _IN_PROGRESS_)
-                count_error         = sum(1 for video_info in self.infoList if video_info.download_status == _ERROR_)
-                self.update_progress(count_done+count_error,N,0)
-                self.dispStatus(f"Processing {N} item(s): Completed downloads {count_done} of {N}      Still in progress = {count_in_progress}, Errors = {count_error}!")
-            #end
 
             # Run in Single thread or multithread mode
             if process_via_multithreading:
-                t = threading.Thread(target=video_item_process_download, args=(n, limits, outputDir, outputExt))
+                t = threading.Thread(target=self.video_item_process_download, args=(n, limits, outputDir, outputExt))
                 t.start()
                 download_threads.append(t)            
             else:
-                video_item_process_download(n, limits, outputDir, outputExt)
+                self.video_item_process_download(n, limits, outputDir, outputExt)
             #end
         #end
 
@@ -142,3 +148,10 @@ class VideoListManager:
                 t.join()
             #end
         #end
+
+    ## -------------------------- Abstract/Template UI functions -----------------------------
+    # These functions are provisioned to be overwritten by the child classes for UI updates.
+    # They are not compulsory to implement.
+    def updateUI_ItemElementStatus(self, videoItem):
+        pass
+
