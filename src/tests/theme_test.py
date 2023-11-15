618017cc954a1b56dfa37ed057c7dec4353cbfd8
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-11-15 22:38:03 +0000

    Fix the prototype test for theme application
    Rename test

--- src/tests/theme_test.py
+++ src/tests/theme_test.py
@@ -1,24 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 def apply_dark_theme(root, theme):
-    root.configure(bg=theme['bg'])
-
-    ttk.Style().configure("TButton", background=theme['button_bg'], foreground=theme['button_fg'], relief="flat")
-    ttk.Style().map("TButton", background=[("active", theme['button_active_bg'])])
-
-    ttk.Style().configure("TLabel", bg=theme['bg'], foreground=theme['label_fg'])
-    ttk.Style().configure("TEntry", fieldbackground=theme['entry_bg'], foreground=theme['entry_fg'], bg=theme['bg'])
+    style = ttk.Style()
+    style.configure("TFrame", background=theme['bg'])
+    style.configure("TButton", background=theme['button_bg'], foreground=theme['button_fg'], relief="flat")
+    style.map("TButton", background=[("active", theme['button_active_bg'])])
+    style.configure("TLabel", background=theme['bg'], foreground=theme['label_fg'])
+    style.configure("TEntry", fieldbackground=theme['entry_bg'], foreground=theme['entry_fg'])
 
     for child in root.winfo_children():
         widget_type = child.winfo_class()
-        if widget_type in ('Label', 'Button', 'Entry'):
+        if widget_type in ('TLabel', 'TButton', 'TEntry'):
+            pass  # ttk widgets, already styled
+        elif widget_type in ('Label', 'Button', 'Entry'):
             child.configure(bg=theme['bg'], fg=theme['fg'])
-        elif widget_type in ('TLabel', 'TButton', 'TEntry'):
-            pass
         apply_dark_theme(child, theme)
-
 
 def create_widgets(root):
     label = ttk.Label(root, text="Label")
@@ -30,12 +27,15 @@ def create_widgets(root):
     entry = ttk.Entry(root)
     entry.grid(row=2, column=0, padx=5, pady=5)
 
-
 def main():
     root = tk.Tk()
     root.title("Dark Theme Test")
 
-    create_widgets(root)
+    # Create a main frame with the desired background color
+    main_frame = ttk.Frame(root, style="TFrame")
+    main_frame.pack(fill=tk.BOTH, expand=True)
+
+    create_widgets(main_frame)  # Pass the main frame instead of root
 
     dark_theme = {
         "bg": "#222831",
@@ -48,10 +48,9 @@ def main():
         "entry_fg": "#eeeeee"
     }
 
-    apply_dark_theme(root, dark_theme)
+    apply_dark_theme(main_frame, dark_theme)  # Apply theme to main frame
 
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
