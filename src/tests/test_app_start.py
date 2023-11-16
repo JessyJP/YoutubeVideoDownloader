9f51a4b99bfd44b1e729d86b8c69d6132eb54571
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-11-16 00:31:04 +0000

    CLI test with random videos
    Update test.py for testing the CLI mode

--- src/tests/test_app_start.py
+++ src/tests/test_app_start.py
@@ -2,15 +2,42 @@ import subprocess
 
 # Variables
 scriptName = './src/YouTubeDownloader.py';
-quality  = "max"
-url = "https://www.youtube.com/watch?v=6TWJaFD6R2s"
-# url = "https://www.youtube.com/watch?v=1La4QzGeaaQ"
-# url = "https://www.youtube.com/watch?v=7n16Yw51xkI&list=PLLgJJsrdwhPxa6-02-CeHW8ocwSwl2jnu"
-outputDir = "R:/" #"./output/"
+args = []
+args.append('--cli')
+
+# Select mode
+# args.append('--web')
+# args.append('--gui')
+
+# Output dir
+# args.append("--output R:/") #"./output/"
+args.append("-o R:/") #"./output/"
+
+# quality  = "max"
+args.append("-c")
+
+# Arguments URL(s)
+args.append("https://www.youtube.com/watch?v=6TWJaFD6R2s")
+args.append("https://www.youtube.com/watch?v=1La4QzGeaaQ")
+args.append("https://www.youtube.com/watch?v=7n16Yw51xkI&list=PLLgJJsrdwhPxa6-02-CeHW8ocwSwl2jnu")
+args.append("https://www.youtube.com/watch?v=7n16Yw51xkI&list=PLLgJJsrdwhPxa6-02-CeHW8ocwSwl2jnu")
+args.append("https://www.youtube.com/watch?v=7n16Yw51xkI&list=PLLgJJsrdwhPxa6-02-CeHW8ocwSwl2jnu")
+args.append("https://www.youtube.com/watch?v=77GWlSiwKI4" )
+args.append('''https://www.youtube.com/watch?v=77GWlSiwKI4
+            https://www.youtube.com/watch?v=DNsLLrCgK0U
+            qweqr123    
+            https://www.youtube.com/playlist?list=PLbpi6ZahtOH6GomiNz1MJDa2aQOeFiMKH
+            qeweqwe
+            ''')
+args.append("https://www.youtube.com/watch?v=7n16Yw51xkI&list=PLLgJJsrdwhPxa6-02-CeHW8ocwSwl2jnu")
+args.append("https://www.youtube.com/playlist?list=PLbpi6ZahtOH6GomiNz1MJDa2aQOeFiMKH")
+args.append("https://www.youtube.com/watch?v=7n16Yw51xkI&list=PLLgJJsrdwhPxa6-02-CeHW8ocwSwl2jnu")
+args.append("https://www.youtube.com/watch?v=7n16Yw51xkI&list=PLLgJJsrdwhPxa6-02-CeHW8ocwSwl2jnu")
 
 # Define the command to be executed
 command = []
-command.append(['python', scriptName , outputDir, url ])
+command.append(['python', scriptName,*args ])
+# command.append(['python', scriptName , outputDir, url ])
 
 # Execute the command
 for cmd in command:
