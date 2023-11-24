7858e79e4bbf1573b3b2ee779bd578d3dfd45aaf
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-11-24 02:59:07 +0000

    Add the Licence & about comment at the top

--- src/web/server_mgr.py
+++ src/web/server_mgr.py
@@ -1,3 +1,30 @@
+"""
+File: server_mgr.py
+
+Application Name: Youtube Video Downloader
+Description: This application allows users to download videos from YouTube by providing a valid URL.
+The user can analyze the video properties and choose the desired quality before downloading the video.
+
+Author: JessyJP
+Email: your.email@example.com TODO: add later
+Date: April 8, 2023
+
+Copyright (C) 2023 Your Name. All rights reserved.
+
+License:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 # Module imports
 import os
 import sys
