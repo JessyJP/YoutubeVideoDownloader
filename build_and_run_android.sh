959f6ec7ec481539b656ef0a559953fa10f1bf49
JessyJP <49342341+JessyJP@users.noreply.github.com>
2023-12-14 05:56:26 +0000

    Script to create build source subset for android deployment

---
+++ build_and_run_android.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Define the source and destination directories
+SEPARARATOR="----------------------------------------------------"
+SRC_DIR="src"
+DEST_DIR="build_android" # Replace with your actual destination directory
+APK_NAME="yddapp"
+
+RSYNC_OPTS="-av --exclude='*.pyc' --exclude='__pycache__/'"
+
+## ===============================================================
+# Your build and run commands go here
+echo $SEPARARATOR
+echo "Remove previous directory [$DEST_DIR] if it exists."
+echo $SEPARARATOR
+rm -vr $DEST_DIR
+
+
+echo $SEPARARATOR
+echo "Create the anroid build subset."
+echo $SEPARARATOR
+# Create destination directory if it doesn't exist
+mkdir -p "$DEST_DIR"
+
+# Use rsync to copy specific directories and exclude .pyc files
+rsync -av --exclude='*.pyc' --exclude='__pycache__/' "$SRC_DIR/core" "$DEST_DIR/"
+rsync -av --exclude='*.pyc' --exclude='__pycache__/' "$SRC_DIR/mobile_kivy_gui" "$DEST_DIR/"
+rsync -av --exclude='*.pyc' --exclude='__pycache__/' "themes" "$DEST_DIR/"
+# rsync -av --exclude='*.pyc' --exclude='__pycache__/' "images" "$DEST_DIR/"
+rsync -av --exclude='*.pyc' --exclude='__pycache__/' "data" "$DEST_DIR/"
+
+# Copy the main file. Rename the file to main because buldozer expects that as a starting point
+cp -v "$SRC_DIR/YouTubeDownloaderKIVY.py"     "$DEST_DIR/main.py"
+# Copy specific files
+cp -v "buildozer.spec" "$DEST_DIR/" #
+cp -v "config.ini"     "$DEST_DIR/"
+
+# Add any other specific files or directories you need to copy here
+
+## ===============================================================
+# Your build and run commands go here
+echo $SEPARARATOR
+echo "Change working directory to [$DEST_DIR]."
+cd $DEST_DIR
+
+# Your build and run commands go here
+echo "Build and run via buildozer."
+echo $SEPARARATOR
+# buildozer -v android debug
+# buildozer -v android debug deploy run logcat
+# adb install $APK_NAME.apk
+
+echo $SEPARARATOR
+echo "Build and run script done."
+echo $SEPARARATOR
