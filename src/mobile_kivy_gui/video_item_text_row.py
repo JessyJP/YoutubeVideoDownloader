0d2615298cb246fa6b45e0f76525cfa5a8c65f03
JessyJP <49342341+JessyJP@users.noreply.github.com>
2024-01-21 21:03:52 +0000

    Update source code files' licence header

--- src/mobile_kivy_gui/video_item_text_row.py
+++ src/mobile_kivy_gui/video_item_text_row.py
@@ -1,29 +1,32 @@
 """
-File: video_item_text_row.py
-
-Application Name: Youtube Video Downloader
-Description: This application allows users to download videos from YouTube by providing a valid URL.
-The user can analyze the video properties and choose the desired quality before downloading the video.
+YouTube Video Downloader
+Copyright (C) 2023 JessyJP
 
 Author: JessyJP
-Email: your.email@example.com TODO: add later
-Date: April 8, 2023
+Year: 2024
+Description: This script downloads YouTube videos in the specified quality or allows the user to select a quality from the available streams.
 
-Copyright (C) 2023 Your Name. All rights reserved.
+MIT License
 
-License:
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Copyright (c) 2024 JessyJP
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 # Core imports
 from core.download_options import * # updateOutputKeepsStr, MediaSymbols # NOTE:imports the symbol list as well
